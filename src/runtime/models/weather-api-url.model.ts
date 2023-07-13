758bdf6c513dd72bfc543a364f75e3780ebda879
Olha Soroka <olha.soroka@inventorsoft.co>
2023-07-13 11:04:27 +0300

    switch to https

--- src/runtime/models/weather-api-url.model.ts
+++ src/runtime/models/weather-api-url.model.ts
@@ -1,3 +1,3 @@
 export function weatherApiUrl(location: string) {
-	return `http://api.weatherapi.com/v1/current.json?key=1b7ef8ce906843ff9aa194302231207&q=${location}&aqi=yes`
+	return `https://api.weatherapi.com/v1/current.json?key=1b7ef8ce906843ff9aa194302231207&q=${location}&aqi=yes`
 }
